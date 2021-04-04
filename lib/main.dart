84ce0ccee5ed7d75a9f7a3b213adc2401bc777a4
lbdjana23 <ngurahlabda@gmail.com>
2021-04-04 10:33:05 +0800

    menambahkan app bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
-void main() {
-runApp(MyApp());
-}
-class MyApp extends StatelessWidget {
+void main() => runApp(MyApp());class MyApp extends StatelessWidget {
 Widget build(BuildContext context) {
-return MaterialApp(home: Text('hello world'));
+return MaterialApp(
+home: Scaffold(
+appBar: AppBar(
+title: Text('My first App'),
+),
+body: Center(child: Text('Hello world')),
+),
+);
 }
 }
