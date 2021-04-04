5d238c9325f37553a0ff01c0f6d91ddaa24055f2
lbdjana23 <ngurahlabda@gmail.com>
2021-04-04 18:02:25 +0800

    menambahkan widged pertanyaan

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,29 @@
 import 'package:flutter/material.dart';
-void main() => runApp(MyApp());class MyApp extends StatelessWidget {
-Widget build(BuildContext context) {
-return MaterialApp(
-home: Scaffold(
-appBar: AppBar(
-title: Text('My first App'),
-),
-body: Center(child: Text('Hello world')),
-),
-);
-}
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('My first App'),
+          ),
+          body: Column(children: <Widget>[
+            Text(''),
+            RaisedButton(
+              child: Text('Jawaban 1'),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text('Jawaban 2'),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text('Jawaban 3'),
+              onPressed: null,
+            )
+          ])),
+    );
+  }
 }
